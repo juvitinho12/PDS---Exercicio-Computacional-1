d9300ad0d2e581a048cccce41a91f8164069c462
João Vitor <vitor.oliveira@alu.ufc.br>
2024-03-29 16:09:34 -0300

    Nova_Versao

--- Questao1.py
+++ Questao1.py
@@ -16,11 +16,35 @@ def med_mov (x, M1, M2):
 x, Fs = librosa.load('fala_sino.wav', sr=None)
 M1, M2 = 5, 10
 y = med_mov(x, M1, M2)
+
+#Análise no Tempo
 plt.figure()
 plt.plot(x)
 plt.plot(y)
 plt.legend(['Aúdio Original', 'Aúdio Filtrado'])
+plt.xlabel('Tempo')
 plt.show()
+
+#Análise na Frequência
+X = np.fft.fft(x)
+Y = np.fft.fft(y)
+
+# Calcula as frequências correspondentes
+freq = np.fft.fftfreq(len(x), 1/Fs)
+
+X = X[:len(X)//2]
+Y = Y[:len(Y)//2]
+freq = freq[:len(freq)//2]
+
+# Gráfico na frequência
+plt.figure()
+plt.plot(freq, np.abs(X))
+plt.plot(freq, np.abs(Y))
+plt.legend(['FFT Original', 'FFT Filtrado'])
+plt.xlabel('Frequência (Hz)')
+plt.show()
+
+# Rodar o áudio
 sd.play(x, Fs)
 sd.wait()
 sd.play(y, Fs)
--- Questao2.py
+++ Questao2.py
@@ -14,13 +14,36 @@ def dif_tras (x):
 
 x, Fs = librosa.load('fala_sino.wav', sr=None)
 y = dif_tras(x)
+
+#Análise no Tempo
 plt.figure()
 plt.plot(x)
 plt.plot(y)
 plt.legend(['Aúdio Original', 'Aúdio Filtrado'])
+plt.xlabel('Tempo')
 plt.show()
+
+#Análise na Frequência
+X = np.fft.fft(x)
+Y = np.fft.fft(y)
+
+# Calcula as frequências correspondentes
+freq = np.fft.fftfreq(len(x), 1/Fs)
+
+X = X[:len(X)//2]
+Y = Y[:len(Y)//2]
+freq = freq[:len(freq)//2]
+
+# Gráfico na frequência
+plt.figure()
+plt.plot(freq, np.abs(X))
+plt.plot(freq, np.abs(Y))
+plt.legend(['FFT Original', 'FFT Filtrado'])
+plt.xlabel('Frequência (Hz)')
+plt.show()
+
+# Rodar o áudio
 sd.play(x, Fs)
 sd.wait()
 sd.play(y, Fs)
 sd.wait()
-
